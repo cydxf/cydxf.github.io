fd5ef7d60167699dd10acaf2a3901b0bff526740
cydxf <2814489090@qq.com>
2024-08-01 18:54:04 +0800

    移除Hexo框架，重新搭建博客

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll("nav ul li a");
+
+    links.forEach(link => {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            const targetSection = document.getElementById(targetId);
+            window.scrollTo({
+                top: targetSection.offsetTop,
+                behavior: "smooth"
+            });
+        });
+    });
+});
